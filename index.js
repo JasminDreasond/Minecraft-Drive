0770f221d25fab46453ac606e6c5bbbc41fecdac
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-07-16 17:25:32 -0300

    index console added

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const path = require('path');
 const fs = require('fs');
 const ini = require('ini');
 const ScriptServer = require('scriptserver');
+console.log('Starting App...');
+
+// Files
 const rootPath = path.dirname(process.execPath);
+console.log(`App Path: ${rootPath}`);
+console.log(`Loading Config...`);
 const tinyCfg = ini.parse(fs.readFileSync(path.join(rootPath, './mine-drive.ini'), 'utf-8'));
+console.log(`Config Loaded!`);
 
 // Module Config
 const config = {
@@ -15,7 +21,10 @@ const config = {
     }
 };
 
+console.log(`Core Config of ScriptServer Loaded!`);
+
 // Custom Config
+console.log(`Loading Custom Config...`);
 if (tinyCfg.custom) {
 
     // String
@@ -28,13 +37,18 @@ if (tinyCfg.custom) {
 
 }
 
+console.log(`Custom Config loaded!`);
+
 // Prepare Minecraft
 const minecraft = { config: config, path: rootPath };
+console.log(`Minecraft values ready!`);
 
 // Custom Start
+console.log(`Starting custom JS File...`);
 let customIndex = null;
 try { customIndex = require(path.join(rootPath, './mine-drive.js')); } catch (err) { customIndex = null; }
-if (typeof customIndex === "function") { customIndex(minecraft); }
+if (typeof customIndex === "function") { customIndex(minecraft); console.log(`Custom JS File started!`); } else { console.log(`Custom JS File not found!`); }
 
 // Start Server
+console.log(`Starting Minecraft Server...`);
 minecraft.server = new ScriptServer(config);
