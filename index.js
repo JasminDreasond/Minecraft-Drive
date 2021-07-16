4d09032b15cbb7cb37f66ea45c9feb74d539e5a0
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-07-16 16:04:29 -0300

    package

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 const package = require('./package.json');
 const path = require('path');
 const fs = require('fs');
+const ini = require('ini');
 const rootPath = path.dirname(process.execPath);
+
+// Custom Start
+let customIndex = null;
+try { customIndex = require(path.join(rootPath, './mine-drive.js')); } catch (err) { customIndex = null; }
+if (typeof customIndex === "function") { customIndex(); }
