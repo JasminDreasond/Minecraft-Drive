783fb9f72c40c215ebb6f1ea5e39d368cf83efac
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-07-16 16:16:25 -0300

    index more text

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const ScriptServer = require('scriptserver');
 const rootPath = path.dirname(process.execPath);
 const tinyCfg = ini.parse(fs.readFileSync(path.join(rootPath, './mine-drive.ini'), 'utf-8'));
 
+const config = {
+    core: {
+        jar: tinyCfg.jar,
+        args: tinyCfg.args,
+        rcon: tinyCfg.rcon
+    }
+};
+
+// Server
+const server = new ScriptServer(config);
+
 // Custom Start
 let customIndex = null;
 try { customIndex = require(path.join(rootPath, './mine-drive.js')); } catch (err) { customIndex = null; }
-if (typeof customIndex === "function") { customIndex(); }
-
-// Server
-const server = new ScriptServer({
-    core: {
-        jar: __dirname + '/' + tinyCfg.java,
-        args: tinyCfg.args,
-        rcon: tinyCfg.rcon,
-        flavorSpecific: tinyCfg.flavorSpecific
-    }
-});
+if (typeof customIndex === "function") { customIndex(server); }
