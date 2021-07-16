afe9e604c4652c6dc1870d885f27b1f240891c56
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-07-16 16:14:24 -0300

    preparing files

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const package = require('./package.json');
 const path = require('path');
 const fs = require('fs');
 const ini = require('ini');
+const ScriptServer = require('scriptserver');
 const rootPath = path.dirname(process.execPath);
+const tinyCfg = ini.parse(fs.readFileSync(path.join(rootPath, './mine-drive.ini'), 'utf-8'));
 
 // Custom Start
 let customIndex = null;
 try { customIndex = require(path.join(rootPath, './mine-drive.js')); } catch (err) { customIndex = null; }
 if (typeof customIndex === "function") { customIndex(); }
+
+// Server
+const server = new ScriptServer({
+    core: {
+        jar: __dirname + '/' + tinyCfg.java,
+        args: tinyCfg.args,
+        rcon: tinyCfg.rcon,
+        flavorSpecific: tinyCfg.flavorSpecific
+    }
+});
